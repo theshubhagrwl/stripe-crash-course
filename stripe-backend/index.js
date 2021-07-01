5ae045e37631cb9d327b70d20579c8af41ed77bf
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-07-01 08:42:22 +0530

    backend setup

--- stripe-backend/index.js
+++ stripe-backend/index.js
@@ -1,21 +1,55 @@
-const cors = require('cors')
-const express = require('express')
-// const stripe = require('stripe')("")
-const { v4: uuidv4 } = require('uuid');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+//Private key
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const { v4: uuidv4 } = require("uuid");
 
-
-const app = express()
-
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 //routes
-app.get("/", (req, res)=> {
-    res.send("It WORKS")
-})
+app.get("/", (req, res) => {
+  res.send("It WORKS");
+});
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("PRODUCT", product);
+  console.log("PRICE", product.price);
+  const idempotency_key = uuidv4();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: product.name,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempotency_key }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
 
 //listen
-app.listen(8000, ()=> {console.log("Listeng @ PORT 8000")})
+app.listen(8000, () => {
+  console.log("Listening @ PORT 8000");
+});
